e625edc236221e28c92fd67825a9b8ad2cb9913c
uvorbs <yuval@orbs.com>
2022-08-07 14:17:26 +0300

    add tick status

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,29 @@
+import * as fs from "fs";
+
 async function sleep(ms: number) {
-  return new Promise((res) => setTimeout(() => res(null), ms));
+    return new Promise((res) => setTimeout(() => res(null), ms));
 }
 
+let status = {
+    Status: "This is the new manager",
+    Payload: {
+        TickCount: 0,
+    },
+};
 async function main() {
-  while (true) {
-    console.log("wait 10 sec");
-    await sleep(1000 * 10);
-  }
+    const statusDir = "./manager/";
+    if (!fs.existsSync(statusDir)) {
+        fs.mkdirSync(statusDir, { recursive: true });
+    }
+    while (true) {
+        status.Payload.TickCount++;
+        console.log("wait 10 sec, tick:", status.Payload.TickCount);
+
+        // write status
+        fs.writeFileSync(statusDir + "status.json", JSON.stringify(status));
+
+        await sleep(1000 * 10);
+    }
 }
 
 //if (require.main === module) {
